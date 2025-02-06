c2c6d582a8af7e5e52a6813264eb59fa20379639
unknown <misaelzion@gmail.com>
2025-02-05 18:32:56 -0600

    agregar detalles al product detail

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -2,17 +2,30 @@ import { createContext, useState } from "react"
 export const ShoppingCartContext = createContext();
 
 export const ShoppingCartProvider  = ({children}) => {
+    // Shopping Card quantity
     const [count,setCount]= useState(0);
+    // Product Detail Open/Close
     const [isProductDetailOpen,setIsProductDetailOpen] = useState(false);
     const openProductDetail = () => setIsProductDetailOpen(true);
     const closeProductDetail = () => setIsProductDetailOpen(false);
+    // Product Detail Shos product
+    const [productToShow, setProductToShow] = useState({
+        title: "",
+        price: "",
+        description: "",
+        images: [],  //Para evitar problema de que no lo encuentre
+      });
+
+
     return (
         <ShoppingCartContext.Provider value={{
             count,
             setCount,
             openProductDetail,
             closeProductDetail,
-            isProductDetailOpen
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow
         }}>
             {children}
         </ShoppingCartContext.Provider>   
