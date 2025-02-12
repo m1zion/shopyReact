47a23954729b2f55cda431089e1e94791449a188
unknown <misaelzion@gmail.com>
2025-02-11 19:19:44 -0600

    agregando orders

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -2,14 +2,31 @@ import { useContext } from "react";
 import { ShoppingCartContext } from '../../Context'
 import Layout from "../../Components/Layout"
 import OrderCard from "../../Components/OrderCard";
+import { ChevronLeftIcon } from "@heroicons/react/24/solid";
+import { Link } from 'react-router-dom'
 function MyOrder() {
     const context = useContext(ShoppingCartContext);
+    const currentPath = window.location.pathname;
+    let index = currentPath.substring(currentPath.lastIndexOf('/')+1);
+    if (index === 'last'){
+        index = context.order?.length - 1;
+    }
     return (
         <Layout>
-            MyOrder
-            <div className='px-6 overflow-y-scroll flex-1'>
+            <div className='relative justify-center flex items-center w-80 mb-6'>
+                <Link
+                to='/my-orders'
+                className='absolute left-0'>
+                    <ChevronLeftIcon 
+                        className='size-6 text-black cursor-pointer'
+                        onClick={context.closeCheckoutSideMenu}
+                    />
+                </Link>
+                <h1>My Order</h1>
+            </div> 
+            <div className='flex flex-col w-110'>
                 {
-                context.order?.slice(-1)[0].products.map(product => (  //Aqui me va a traer el ultimo elemento
+                context.order?.[index]?.products?.map(product => (  //Aqui me va a traer el ultimo elemento
                     <OrderCard 
                         id={product.id}
                         title={product.title} 
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,8 +1,30 @@
+import { useContext } from 'react'
 import Layout from "../../Components/Layout"
+import { ShoppingCartContext } from '../../Context';
+import OrdersCard from "../../Components/OrdersCard"
+import { Link } from 'react-router-dom'
+import { ChevronLeftIcon } from '@heroicons/react/24/solid'
 function MyOrders() {
+    const context = useContext(ShoppingCartContext);
     return (
         <Layout>
-            MyOrders
+            <div className='relative justify-center flex items-center w-80'>
+                <h1>My Orders</h1>
+            </div>            
+            {
+                context.order.map((order,index) =>(
+                    <Link 
+                    key={index}
+                    to={`/my-orders/${index}`}
+                    >
+                        <OrdersCard                        
+                        totalPrice={order.totalPrice}
+                        totalProduct={order.totalProducts}
+                        />
+                    </Link>
+                )
+                )
+            }
         </Layout>
     )
   }
